d8a9e9390de679caae9412fb833e6cc10678d538
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2023-12-18 18:26:39 +0530

    yarn add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
-import { useState } from "react";
-import BoardForms from "./components/BoardForms";
-import { Board } from "./types/board";
-import BoardItem from "./components/BoardItem";
-
-type State = {
-	boards: Board[];
-};
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 function App() {
-	const [state, setState] = useState<State>({ boards: [] });
-
-	const addBoard = (title: string) => {
-		console.log("addBoard called with Title: ", title);
-		const newBoard = {
-			id: crypto.randomUUID(),
-			title: title,
-		};
-		setState({ boards: [...state.boards, newBoard] });
-	};
-
-	const deleteBoard = (id: String) => {
-		console.log("deleteBoard called with ID: ", id);
-		const newBoards = state.boards.filter((board) => board.id !== id);
-		setState({ boards: newBoards });
-	};
 	return (
-		<div className="h-screen max-w-2xl mx-auto bg-gray-200 p-6">
-			<BoardForms addBoard={addBoard} />
-			{state.boards.map((board) => (
-				<BoardItem
-					id={board.id}
-					title={board.title}
-					key={board.id}
-					deleteBoard={deleteBoard}
-				/>
-			))}
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
+		</Router>
 	);
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,41 @@
+import BoardForms from "@/components/BoardForms";
+import BoardItem from "@/components/BoardItem";
+import { Board } from "@/types/board";
+import { useState } from "react";
+
+
+type State = {
+	boards: Board[];
+};
+function HomePage() {
+	const [state, setState] = useState<State>({ boards: [] });
+
+	const addBoard = (title: string) => {
+		console.log("addBoard called with Title: ", title);
+		const newBoard = {
+			id: crypto.randomUUID(),
+			title: title,
+		};
+		setState({ boards: [...state.boards, newBoard] });
+	};
+
+	const deleteBoard = (id: String) => {
+		console.log("deleteBoard called with ID: ", id);
+		const newBoards = state.boards.filter((board) => board.id !== id);
+		setState({ boards: newBoards });
+	};
+	return (
+		<div className="h-screen max-w-2xl mx-auto bg-gray-200 p-6">
+			<BoardForms addBoard={addBoard} />
+			{state.boards.map((board) => (
+				<BoardItem
+					id={board.id}
+					title={board.title}
+					key={board.id}
+					deleteBoard={deleteBoard}
+				/>
+			))}
+		</div>
+	);
+}
+export default HomePage;
